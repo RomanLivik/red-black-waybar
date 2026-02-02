1d1e2414859b3fb5791ad9f24bf72e77c7304483
S0LY3R <114293450+RomanLivik@users.noreply.github.com>
2026-02-02 17:59:02 +0300

    Delete scripts directory because fail

--- scripts/notif_history.sh
+++
@@ -1,7 +0,0 @@
-history=$(dunstctl history)
-
-if [ -z "$history" ] || [ "$history" == '{"data": [[]]}' ]; then
-    echo "[]"
-else
-    echo "$history" | jq '.data[0] | .[0:5] | map({summary: .summary.data, body: .body.data})'
-fi
--- scripts/wifi_info.sh
+++
@@ -1,1 +0,0 @@
-nmcli -t -f "ACTIVE,SSID,SIGNAL,BARS" dev wifi list | sed 's/\\:/_/g' | awk -F: '{print "{\"active\":\""$1"\",\"ssid\":\""$2"\",\"signal\":\""$3"\",\"bars\":\""$4"\"}"}' | jq -s '.' || echo "[]"
